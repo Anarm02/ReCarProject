c200eaa047d3ae5b7c3e0214dcc04b008e258f0d
Anar_Mammadli <mammadli.anar2002@gmail.com>
2024-03-14 14:30:05 +0400

    Adding autofac

---
+++ Business/DependencyResolver/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Concrete;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
+using DataAccess.EntityFramework;
+
+namespace Business.DependencyResolver.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
+            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+            builder.RegisterType<CustomerManager>().As<ICustomerService>().SingleInstance();
+            builder.RegisterType<EfCustomerDal>().As<ICustomerDal>().SingleInstance();
+            builder.RegisterType<RentalManager>().As<IRentalService>().SingleInstance();
+            builder.RegisterType<EfRentalDal>().As<IRentalDal>().SingleInstance();
+            builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
+            builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();  
+        }
+    }
+}
