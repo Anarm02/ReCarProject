f32d611b28212e20bbedcc35e4e9f06fffc43f74
Anar_Mammadli <mammadli.anar2002@gmail.com>
2024-03-31 21:26:52 +0400

    Add login service

---
+++ Business/Concrete/AuthManager.cs
@@ -0,0 +1,62 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Core.Utilities.Security.Hashing;
+using Core.Utilities.Security.JWT;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AuthManager : IAuthService
+    {
+        IUserService _userService;
+        ITokenHelper _tokenHelper;
+        public AuthManager(ITokenHelper tokenHelper, IUserService userService)
+        {
+            _tokenHelper = tokenHelper;
+            _userService = userService;
+        }
+        public IDataResult<AccesToken> CreateAccessToken(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<User> Login(UserForLoginDto userForLoginDto)
+        {
+            var userToCheck = _userService.GetByEmail(userForLoginDto.Email);
+            if (userToCheck.Data == null)
+            {
+                return new ErrorDataResult<User>(Messages.userNotExist);
+            }
+            else if (!HashingHelper.VerifyPasswordHash(userForLoginDto.Password, userToCheck.Data.PasswordHash, userToCheck.Data.PasswordSalt))
+            {
+                return new ErrorDataResult<User>(Messages.PasswordError);
+            }
+            else
+            {
+                return new SuccessDataResult<User>(userToCheck.Data,Messages.SuccessfullLogin);
+            }
+        }
+
+        public IDataResult<User> Register(UserForRegisterDto userForRegisterDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult UserExist(string email)
+        {
+            var userToCheck= _userService.GetByEmail(email);    
+            if (userToCheck.Data != null)
+            {
+                return new ErrorResult(Messages.UserAlreadyExist);
+            }
+            return new SuccessResult(Messages.userNotExist);
+        }
+    }
+}
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ namespace Business.Constants
         public static string nameInvalid = "Isimlendirmede hata var";
         public static string isDeleted = "Araba listeden silindi";
         public static string isUpdated = "Arab guncellendi";
-        internal static string CarImagesCount="Urune daha fazla resim eklenemez";
+        public static string CarImagesCount="Urune daha fazla resim eklenemez";
+        public static string  userNotExist="Boyle bir kullanici yok";
+        public static string PasswordError="Kod Yanlis";
+        public static string SuccessfullLogin="Hesaba Giris yapildi";
+        public static string UserAlreadyExist="Kullanici zaten mevcut";
     }
 }
